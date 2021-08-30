ab100b54ee029ee9c1d3d150b94ec7d1d5788e2d
shaochangxu.scx <shaochangxu.scx@antgroup.com>
2021-08-30 09:53:58 +0800

    view select

--- src/mvs/model.h
+++ src/mvs/model.h
@@ -39,6 +39,7 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <Eigen/Core>
 
 #include "mvs/depth_map.h"
 #include "mvs/image.h"
@@ -87,8 +88,8 @@ struct Model {
       const float percentile = 50) const;
 
   // Compute the view ray and pos of each view
-  std::unordered_map<int, Eigen::Vector3f> Model::ComputeViewRays() const;
-  std::unordered_map<int, Eigen::Vector3f> Model::ComputeViewPos() const;
+  std::unordered_map<int, Eigen::Vector3f> ComputeViewRays() const;
+  std::unordered_map<int, Eigen::Vector3f> ComputeViewPos() const;
 
   // Note that in case the data is read from a COLMAP reconstruction, the index
   // of an image or point does not correspond to its original identifier in the
