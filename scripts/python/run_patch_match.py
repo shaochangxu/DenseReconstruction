ab7090a6e5327dbb5767c9db500853733213a9f5
shaochangxu.scx <shaochangxu.scx@antgroup.com>
2021-08-30 19:48:02 +0800

    add modify_cfg

---
+++ scripts/python/run_patch_match.py
@@ -0,0 +1,10 @@
+import os
+import os.path
+
+build_path = "/home/hadoop/scx/buaa/build/src/exe/colmap "
+dense_dir = "/home/hadoop/scx/buaa/test_data/ws_scan9/dense/0/"
+ply_path = "/home/hadoop/scx/buaa/test_data/ws_scan9/dense/0/scan9.ply"
+
+#os.system("python /home/hadoop/scx/buaa/DenseReconstruction/scripts/python/modify_cfg.py --cfg_dir " + dense_dir + "/stereo"+ " --view_select_algo __two-stage__,3,7")
+os.system(build_path + "patch_match_stereo --workspace_path " + dense_dir + " --workspace_format COLMAP --PatchMatchStereo.geom_consistency true")
+os.system(build_path + "stereo_fusion --workspace_path " + dense_dir + " --workspace_format COLMAP --input_type geometric --output_path " + ply_path)
